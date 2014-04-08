80dbbb7c5ad3aa14904554999ec3358886f8a046
Andy <Andy.at.working@gmail.com>
2014-04-08 15:25:35 +0800

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+tbox
+===
+---
+
+###NAME
+> tbox - tool box，我的工具箱
+
+###DESCRIPTION
+> 这里都是我coding是突然想起来写的一些测试用的代码
+
---
+++ get_socket_client_ip.c
@@ -0,0 +1,55 @@
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netdb.h>
+#include<string.h>
+
+
+int open_listenfd(int port)
+{
+	int listenfd,optval = 1;
+	struct sockaddr_in serveraddr;
+
+	if((listenfd = socket(AF_INET,SOCK_STREAM,0)) < 0)
+		return -1;
+
+	/*Eliminates "Address already in use" error from blind*/
+	if(setsockopt(listenfd,SOL_SOCKET,SO_REUSEADDR,
+		      (const void *)&optval,sizeof(int)) < 0)
+		return -1;
+
+	bzero((char *) &serveraddr,sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serveraddr.sin_port = htons((unsigned short)port);
+	if(bind(listenfd,(struct sockaddr *)&serveraddr,sizeof(serveraddr)) < 0)
+			return -1;
+	if(listen(listenfd,SOMAXCONN) < 0)
+		return -1;
+	return listenfd;
+}
+
+int main()
+{
+	int fd = open_listenfd(4900);
+	struct sockaddr_in addr;
+	socklen_t len = sizeof(addr); //这里一定要初始化为sizeof(struct sockaddr)
+
+	bzero((char*)&addr, sizeof(addr));
+	int client = accept(fd, (struct sockaddr *) &addr, &len);
+
+	//已经可以拿到client的addr了，下面的代码纯粹是我无聊，测试一下两个函数的区别
+	//这里在编译的时候会有一个警告，关于int转char*的，无视之
+	printf("%s\n", inet_ntoa(addr.sin_addr));
+	getsockname(client, (struct sockaddr *) &addr, &len);
+	printf("%s\n", inet_ntoa(addr.sin_addr));
+	getpeername(client, (struct sockaddr *) &addr, &len);
+	printf("%s\n", inet_ntoa(addr.sin_addr));
+
+    char buff[256] = {'\0'};//just for pause
+	read(client, &buff, 256);
+	close(client);
+	close(fd);
+	
+	return 0;
+}
